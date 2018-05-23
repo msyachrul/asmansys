e1e332dd6fb0c01b9749d56e8fd0926129f1f74b
MSL <abdisyachrul@gmail.com>
2018-05-23 10:45:50 +0700

    ubah icon

--- resources/views/layout/layouts.blade.php
+++ resources/views/layout/layouts.blade.php
@@ -7,8 +7,8 @@
     <meta name="description" content="Sufee Admin - HTML5 Admin Template">
     <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
 
-    <link rel="apple-touch-icon" href="apple-icon.png">
-    <link rel="shortcut icon" href="favicon.ico">
+    <link rel="apple-touch-icon" href="{{ asset('images/icon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('images/icon.png') }}">
 
     <link rel="stylesheet" href="{{ asset('assets/css/normalize.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
